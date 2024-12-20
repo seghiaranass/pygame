98654da7779acb6e49bfe76e74690b80c02464d2
seghiaranass <anass.segh97@gmail.com>
2024-12-20 11:08:33 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame 
+import time 
+import random 
+
+WIDTH, HEIGHT = 1000, 800 
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+pygame.display.set_caption("Space Dodge")
+
+BG = pygame.transform.scale(pygame.image.load("bg.jpeg"),(WIDTH, HEIGHT))
+
+def draw():
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
+
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        draw()
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+        
